61138fc45718050418eab8e5888c0edf5c72d69b
jbaizack <jbaizack222@gmail.com>
2025-06-25 09:30:15 -0500

    Included the base path to image

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const cardInfo = {
   name: "Sarah Dole",
   username: "@sarahdole", 
   review: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
-  imagePath: "profile-thumbnail.png"
+  imagePath: "/testimonial-card/profile-thumbnail.png"
 }
 export default function App(): React.ReactNode {
   return <Card info={cardInfo}/>
