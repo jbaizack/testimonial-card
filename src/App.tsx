69b65badea380fe7ad233aa0e8883dd44ae49996
jbaizack <jbaizack222@gmail.com>
2025-06-25 09:09:42 -0500

    Added a public folder for the image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react"; 
 import Card from "./components/Card"
-// const name = "Sarah Dole"; 
-// const userName = "@sarahdole"; 
-// const review = "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!";
+
 
 const cardInfo = {
   name: "Sarah Dole",
   username: "@sarahdole", 
   review: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
-  imagePath: "img/profile-thumbnail.png"
+  imagePath: "profile-image.png"
 }
 export default function App(): React.ReactNode {
   return <Card info={cardInfo}/>
